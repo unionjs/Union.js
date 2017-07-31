66c0144c7b63b0bb52c3da60b218c55cffe1ba65
荒川友人 <u.jin.arakawa@gmail.com>
2017-07-31 19:26:08 +0900

    change zip file

---
+++ union-beta/union.js
@@ -0,0 +1,65 @@
+/*!
+ * Union.js
+ * https://unionjs.github.io/app/
+ * Version: Public Beta
+ *
+ * Copyright 2017 Tomohito Arakawa
+ * Released under the MIT license
+ * https://github.com/unionjs/Union.js/blob/master/LICENSE
+ */
+(function($){
+
+  $.fn.union = function(option) {
+
+    var setting = $.extend({
+      'target': $(this),
+      'url': '',
+      'duration': '.5s',
+      'easing': 'ease',
+      'setTimeout': '1000'
+    }, option);
+
+    $.ajax({
+      type: 'GET',
+      url: setting.url
+    }).done(function(data){
+      var svg = $(data).find('svg');
+      svg.css({
+        'overflow': 'visible'
+      })
+      $.each(svg.find('*'), function(){
+        var randomRorate = Math.floor(Math.random() * 361),
+        height = $(window).height(),
+        width = $(window).width(),
+        minY = height * -1,
+        maxY = height * 2,
+        minX = width * -1,
+        maxX = width * 2,
+        randomY = Math.floor(Math.random() * maxY) + minY,
+        randomX = Math.floor(Math.random() * maxX) + minX;
+        $(this).css({
+          'oTransitionDuration': setting.duration,
+          'webkitTransitionDuration': setting.duration,
+          'transitionDuration': setting.duration,
+          'oTransitionTimingFunction': setting.easing,
+          'webkitTransitionTimingFunction': setting.easing,
+          'transitionTimingFunction': setting.easing,
+          'msTransform': 'translate(' + randomX + 'px , ' + randomY + 'px)',
+          'webkitTransform': 'translate(' + randomX + 'px , ' + randomY + 'px)',
+          'transform': 'translate(' + randomX + 'px , ' + randomY + 'px)',
+        });
+      });
+      setting.target.prepend(svg);
+    });
+
+    setTimeout(function(){
+      setting.target.find('svg *').css({
+        'msTransform': 'translate(' + 0 + 'px , ' + 0 + 'px)',
+        'webkitTransform': 'translate(' + 0 + 'px , ' + 0 + 'px)',
+        'transform': 'translate(' + 0 + 'px , ' + 0 + 'px)',
+      })
+    },setting.setTimeout);
+
+  };
+
+})(jQuery);
